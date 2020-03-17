1db6b40ffc413fb589b3d68930068dd2cd99841b
Marlo <marlo.baguyos@outlook.com>
2020-03-17 14:49:28 -0400

    update

--- Lab1/myMath.js
+++ Lab1/myMath.js
@@ -5,5 +5,14 @@ module.exports = {
 
     sum: function(num1, num2) {
         return num1 + num2;
+    },
+    greater: function(num1, num2){
+        //return the greater of num1 and num2
+        if (num1 > num2) {
+            return num1;
+        }
+        else {
+            return num2;
+        }
     }
 };
