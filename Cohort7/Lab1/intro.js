96d9f482f148b4fbfff2109309edb227f30a734a
Marlo <marlo.baguyos@outlook.com>
2020-04-27 14:00:24 -0400

    update

--- Cohort7/Lab1/intro.js
+++ Cohort7/Lab1/intro.js
@@ -1,27 +1,36 @@
 var mathLib = require('./myMath');
 
+function homework(){
+    for(var i = 0; i<40; i++){
+        if(i%2 === 0)
+        console.log(i)
+    }
+}
+
 function init() {
-console.log('Hello World!');
+// console.log('Hello World!');
 
-let age = 90;
-console.log(age);
+// let age = 90;
+// console.log(age);
 
-let name = "Marlo Baguyos";
-console.log("My name is: " + name);
+// let name = "Marlo Baguyos";
+// console.log("My name is: " + name);
 
-mathLib.sayHello("a parameter value"); // exec the fn on the module
+// mathLib.sayHello("a parameter value"); // exec the fn on the module
 
-var res = mathLib.sum(21,21);
-console.log(res);
+// var res = mathLib.sum(21,21);
+// console.log(res);
 
-var g=mathLib.greater(897123, 89123);
-console.log(g);
+// var g=mathLib.greater(897123, 89123);
+// console.log(g);
 
-console.log(mathLib.greater(-1, -1));
+// console.log(mathLib.greater(-1, -1));
 
-console.log(mathLib.isEven(4));
+// console.log(mathLib.isEven(10));
 
-console.log(mathLib.min(5, 2));
+// console.log(mathLib.min(5, 2));
+
+homework();
 }
 
 init();
