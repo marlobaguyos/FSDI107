81b635eeddb62807de9363e0bd313d98bd5ca74b
Marlo <marlo.baguyos@outlook.com>
2020-04-27 13:38:05 -0400

    update

--- Cohort7/Lab1/intro.js
+++ Cohort7/Lab1/intro.js
@@ -1,3 +1,4 @@
+var mathLib = require('./myMath');
 
 function init() {
 console.log('Hello World!');
@@ -7,6 +8,18 @@ console.log(age);
 
 let name = "Marlo Baguyos";
 console.log("My name is: " + name);
+
+mathLib.sayHello("a parameter value"); // exec the fn on the module
+
+var res = mathLib.sum(21,21);
+console.log(res);
+
+var g=mathLib.greater(897123, 89123);
+console.log(g);
+
+console.log(mathLib.greater(-1, -1));
+
+console.log(mathLib.isEven(4));
 }
 
 init();
---
+++ Cohort7/Lab1/myMath.js
@@ -0,0 +1,22 @@
+module.exports = {
+    sayHello: function(text){
+        console.log('Hello there!' + text);
+    },
+    sum: function(num1, num2){
+        return num1 + num2;
+    },
+    greater: function(num1, num2){
+        //return the greater of num1 and num2
+        if(num1 > num2) {
+            return num1;
+        }
+        return num2;
+    },
+    isEven: function(num){
+        if(num%2 != 0){
+            return("Your number is ODD")
+        }
+        return("Your number is EVEN")
+    }
+    
+};
