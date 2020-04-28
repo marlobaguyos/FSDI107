c877b28da0eec3053fb01308dfe9e6d3bf65eb6a
Marlo <marlo.baguyos@outlook.com>
2020-04-27 20:50:16 -0400

    update

--- Cohort7/Project/server.js
+++ Cohort7/Project/server.js
@@ -1,8 +1,37 @@
 var http = require('http');
 var express = require('express');
 
+
+/*******************************************************
+ *  Configuration section
+********************************************************/
 var app = express();
+
+
+
+
+/*******************************************************
+ *  Web Server Functionality
+********************************************************/
+
+app.get('/', function(req, res){
+    console.log("Req on root page");
+    res.send("<h1 style='color:red;'>Hello World!</h1>");
+});
+
+app.get('/about', function(req, res){
+    res.send("I'm Marlo Baguyos");
+});
 
 app.listen(8080, function() {
     console.log("Server running at localhost:8080");
 });
+
+/*******************************************************
+ *  API functionality
+********************************************************/
+
+app.get('/API/catalog', function(req, res){
+    var data = [];
+    res.json(data);
+})
